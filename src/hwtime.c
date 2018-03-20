6b966390b25f6ddfd1e7e14282d60b342cc2c3ad
till straumann <till.straumann@alumni.tu-berlin.de>
2018-03-20 09:18:18 -0700

    Removed EVR ErGetTicks(); always use clock_gettime

--- src/hwtime.c
+++ src/hwtime.c
@@ -1,38 +1,17 @@
 #include <drvPadUdpComm.h>
 
-#ifndef TEST_ONLY
-#if defined(__rtems__)
-#include <drvMrfEr.h>
-#else
-#include <devMrfEr.h>
-#endif
-
-DrvPadUdpCommHWTime
-drvPadUdpCommHWTime(void)
-{
-epicsUInt32 now;
-	ErGetTicks(0, &now);
-	return now;
-}
-
-DrvPadUdpCommHWTime
-drvPadUdpCommHWBaseTime(void)
-{
-	return 0;
-}
-
-#else
-
 #include <time.h>
 
 DrvPadUdpCommHWTime
 drvPadUdpCommHWTime_default(void)
 {
 struct timespec now;
-uint64_t now_l;
+DrvPadUpdCommHWTime       now_ns;
+const DrvPadUdpCommHWTime ns = (DrvPadUdpCommHWTime)1000000000ULL;
+
 	clock_gettime( CLOCK_MONOTONIC, &now );
-	now_l = ((uint64_t)now.tv_sec) * 1000000000ULL + now.tv_nsec;
-	return (DrvPadUdpCommHWTime)now_l;
+	now_ns = ((DrvPadUdpCommHWTime)now.tv_sec) * ns + (DrvPadUdpCommHWTime)now.tv_nsec;
+	return now_ns;
 }
 
 DrvPadUdpCommHWTime
@@ -42,5 +21,3 @@ __attribute__((weak, alias("drvPadUdpCommHWTime_default")));
 DrvPadUdpCommHWTime
 drvPadUdpCommHWBaseTime(void)
 __attribute__((weak, alias("drvPadUdpCommHWTime_default")));
-
-#endif
